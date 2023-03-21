a5b8d318091fee2d03066ac6f2f9fabbee8643e2
bandurama <banduram1@gmail.com>
2023-03-21 23:15:10 +0100

    Initial commit

---
+++ src/main/java/pl/fryciarnia/backend/FryciarniaApplication.java
@@ -0,0 +1,13 @@
+package pl.fryciarnia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FryciarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FryciarniaApplication.class, args);
+	}
+
+}
