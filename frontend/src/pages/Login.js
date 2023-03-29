e19a7b5c1ee519d41bcdb49dda20fc90b323011a
bandurama <banduram1@gmail.com>
2023-03-29 19:06:33 +0200

    Panel rejestracji i moje konto

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -17,11 +17,7 @@ export default function Login() {
 	return (
 		<>
 			<TopNav />
-			{/*<p>Login</p>*/}
-			{/*<p>Loginining</p>*/}
-			{/*<a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=268913103926-nvg6lersjlvopnouhesjcvts6ttrj14v.apps.googleusercontent.com&redirect_uri=http://bandurama.ddns.net/gauth&response_type=token&scope=https://www.googleapis.com/auth/userinfo.profile&include_granted_scopes=true&state=pass-through%20value">Goówno OaAuth 2.0</a>*/}
-
-			<div className="wrapper">
+			<div className="formwrapper">
 				<div className="formbox">
 					<div className="formtitle">
 						Zaloguj się
---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,80 @@
+import TopNav from "../components/TopNav";
+import '../styles/Forms.css'
+import {useState} from "react";
+
+export default function Profile ()
+{
+	const [tbxMail, setTbxMail] = useState('');
+	const [tbxName, setTbxName] = useState('');
+	const [tbxSurname, setTbxSurname] = useState('');
+
+	const dispMyProfile = () => (
+		<>
+			<div className="formtitle">
+				Mój Profil
+			</div>
+			<div className="formrow">
+				<input type="text" disabled placeholder="E-Mail" value={tbxMail}/>
+			</div>
+			<div className="formrow">
+				<input type="text" placeholder="Imię" onChange={(e) => setTbxName(e.target.value)}  value={tbxName}/>
+			</div>
+			<div className="formrow">
+				<input type="text" placeholder="Nazwisko"  onChange={(e) => setTbxSurname(e.target.value)} value={tbxSurname}/>
+			</div>
+			<div className="formrow">
+				<button>ZAPISZ</button>
+			</div>
+		</>
+	)
+
+	const dispSettings = () => (
+		<>
+			<div className="formtitle">
+				Ustawienia
+			</div>
+			<div className="formrow">
+				<input type="password" placeholder="Stare hasło" />
+			</div>
+			<div className="formrow">
+				<input type="password" placeholder="Nowe hasło" />
+			</div>
+			<div className="formrow">
+				<input type="password" placeholder="Powtórz nowe hasło" />
+			</div>
+			<div className="formrow">
+				<button>ZMIEŃ HASŁO</button>
+			</div>
+			<div className="formrow">
+				Możesz też
+				<a href="./register">
+					usunąć
+				</a>
+				&nbsp;swoje konto
+			</div>
+		</>
+	)
+	const [mainDisplay, setMainDisplay] = useState(dispMyProfile);
+
+	return (
+		<>
+			<TopNav />
+			<div className="formwrapper">
+				<div className="formbox">
+					<div className="test">
+						<div className="button" onClick={(e) => setMainDisplay(dispMyProfile())}>
+							Mój Profil
+						</div>
+						<div className="button">
+							Historia
+						</div>
+						<div className="button" onClick={(e) => setMainDisplay(dispSettings())}>
+							Ustawienia
+						</div>
+					</div>
+					{mainDisplay}
+				</div>
+			</div>
+		</>
+	)
+}
